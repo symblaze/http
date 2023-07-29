ba07bd693489fb899beda0538a2d8a4d5843ac26
imdhemy <imdhemy@gmail.com>
2023-07-29 17:04:28 +0200

    [tests] add bundle extension tests

---
+++ tests/DependencyInjection/HttpBundleExtensionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symblaze\Bundle\Http\Tests\DependencyInjection;
+
+use Symblaze\Bundle\Http\DependencyInjection\HttpBundleExtension;
+use Symblaze\Bundle\Http\Tests\TestCase;
+use Symblaze\Bundle\Http\Validation\ValidatorInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+final class HttpBundleExtensionTest extends TestCase
+{
+    /** @test */
+    public function it_loads_services_yaml_file(): void
+    {
+        $containerBuilder = new ContainerBuilder();
+        $sut = new HttpBundleExtension();
+
+        $sut->load([], $containerBuilder);
+
+        $this->assertTrue($containerBuilder->has(ValidatorInterface::class));
+    }
+}
