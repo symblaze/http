935c60fa3e4ace75528b2182762642e1ccd6c2fb
Dhemy <imdhemy@gmail.com>
2024-03-17 20:32:32 +0100

    fix(exception): make renderable exception throwable (#39)

--- src/Exception/RenderableExceptionInterface.php
+++ src/Exception/RenderableExceptionInterface.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Symblaze\Bundle\Http\Exception;
 
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 /**
  * All exceptions that should create a response should implement this interface.
  */
-interface RenderableExceptionInterface
+interface RenderableExceptionInterface extends Throwable
 {
     /**
      * Render the exception into a response.
