df2cf971677ef8a86fb09a8582edf6187133bf6f
imdhemy <imdhemy@gmail.com>
2024-03-25 13:38:22 +0200

    chore(tests): add faker instance

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Symblaze\Bundle\Http\Tests;
 
+use Faker\Factory;
+use Faker\Generator as Faker;
 use PHPUnit\Framework\TestCase as PHPUnit;
 
 /**
@@ -11,4 +13,12 @@ use PHPUnit\Framework\TestCase as PHPUnit;
  */
 abstract class TestCase extends PHPUnit
 {
+    protected Faker $faker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = Factory::create();
+    }
 }
